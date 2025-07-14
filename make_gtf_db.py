782ee0039bf53798c38ba7e5ddca5a3b9465b6ef
Ryan <weberrl@uci.edu>
2025-07-13 22:38:33 -0700

    fix

--- make_gtf_db.py
+++ make_gtf_db.py
@@ -1,9 +1,8 @@
 import gffutils
 
 # Constants: adjust paths as needed
-# GTF_FILE = "/Users/ryan/Desktop/variant_streamlit/gencode.vM37.nochr.annotation.CHROM1.gtf"
-GTF_FILE = "/Users/ryan/Desktop/variant_streamlit/files/gencode.vM37.nochr.annotation.gtf"
-DB_FILE = "mm39_gtf.db"
+GTF_FILE = "gencode.vM37.nochr.annotation.gtf"
+DB_FILE = "mm39_gtf_filt.db"
 
 gffutils.create_db(
     data=GTF_FILE,
